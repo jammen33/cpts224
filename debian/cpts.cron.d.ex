505d288272f818d8b8173b7fd6399c096457eab7
Joe Carter <jammen@gmail.com>
2011-12-06 13:59:57 -0800

    removed debian folder for option1

--- debian/cpts.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the cpts package
-#
-0 4	* * *	root	[ -x /usr/bin/cpts_maintenance ] && /usr/bin/cpts_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(cpts):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="cpts" command="/usr/bin/cpts"
